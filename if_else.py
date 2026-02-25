e012de573be072ce9c5574180898304a266a0688
Siddarth <adapasiddarth@gmail.com>
2026-02-25 19:32:30 +0000

    Add files via upload

---
+++ if_else.py
@@ -0,0 +1,12 @@
+a = int(input(" buying price:"))
+b = int(input("selling price:"))
+if a < b :
+   c = b - a 
+   print("profit occured",c)
+else:
+   d = a - b 
+   print("loss occured",d)
+   
+
+
+
