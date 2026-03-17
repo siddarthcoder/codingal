3dba5923823720e0a39b5f20dc13e676da32fc86
Siddarth <adapasiddarth@gmail.com>
2026-03-17 22:23:02 +0000

    Add files via upload

---
+++ alphabets.py
@@ -0,0 +1,10 @@
+char = input("Enter a character: ")
+
+if char >= 'A' and char <= 'Z':
+    print("It is an alphabet")
+
+elif char >= 'a' and char <= 'z':
+    print("It is an alphabet")   
+
+else:
+    print("It is not an alphabet")
