f411e3b2eab1ed38c7508bb10ef9ef19f034e164
Siddarth <adapasiddarth@gmail.com>
2026-05-06 20:13:28 +0100

    Add files via upload

---
+++ star.py
@@ -0,0 +1,12 @@
+print("half a pryamid pattern of stars(*:)")
+n = int(input("enter the number of rows"))
+
+for i in range (n):
+
+    for j in range (i + 1):
+
+        print("*",end="")
+    print()    
+
+
+
